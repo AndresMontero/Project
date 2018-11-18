cb69e0da1b092658263b1b8f1ee6c5ac9bc02131
Ariel <ariel.har17@me.com>
2018-11-18 21:39:44 +0100

    comment matplot inline

--- work.py
+++ work.py
@@ -1,33 +1,33 @@
-import re
-import pandas as pd
-import numpy as np
-import dateutil.parser
-import time
-import matplotlib
-import matplotlib.pyplot as plt
-from random import randint
+# import re
+# import pandas as pd
+# import numpy as np
+# import dateutil.parser
+# import time
+# import matplotlib
+# import matplotlib.pyplot as plt
+# from random import randint
 
 # import findspark
 # findspark.init()
 
-from pyspark.sql import *
-from pyspark.sql.functions import *
-from pyspark.sql.functions import min
+# from pyspark.sql import *
+# from pyspark.sql.functions import *
+# from pyspark.sql.functions import min
 
-from pyspark.sql import SparkSession
-from pyspark import SparkConf, SparkContext
+# from pyspark.sql import SparkSession
+# from pyspark import SparkConf, SparkContext
 
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 
-from datetime import datetime, date
-import seaborn as sns
-from pyspark.sql.functions import array_contains
+# from datetime import datetime, date
+# import seaborn as sns
+# from pyspark.sql.functions import array_contains
+
 
 DATA_DIR = '/datasets/twitter_internetarchive/2017/06/16/00/'
 
-
-posts = spark.read.json(DATA_DIR + '*')
+posts = spark.read.json('hdfs://{dir}{name}'.format(dir=DATA_DIR, name='*'))
 
 # Information of the dataset
 posts.show(10)
