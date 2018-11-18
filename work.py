f9d40b374fbf1b0d695d4b471966cdf63e43c296
Ariel <ariel.har17@me.com>
2018-11-18 21:51:36 +0100

    UTF8

--- work.py
+++ work.py
@@ -1,15 +1,3 @@
-# import re
-# import pandas as pd
-# import numpy as np
-# import dateutil.parser
-# import time
-# import matplotlib
-# import matplotlib.pyplot as plt
-# from random import randint
-
-# import findspark
-# findspark.init()
-
 import sys
 
 from pyspark.sql import *
@@ -22,10 +10,6 @@ from pyspark import SparkConf, SparkContext
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 
-# from datetime import datetime, date
-# import seaborn as sns
-# from pyspark.sql.functions import array_contains
-
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 DATA_DIR = '/datasets/twitter_internetarchive/2017/06/16/00/'
@@ -33,4 +17,4 @@ DATA_DIR = '/datasets/twitter_internetarchive/2017/06/16/00/'
 posts = spark.read.json('hdfs://{dir}{name}'.format(dir=DATA_DIR, name='*'))
 
 # Information of the dataset
-posts.show(10)
+print(posts.show(10))
