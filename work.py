4029c90502e45a481b4b5ac250b01134a9455a84
Ariel <ariel.har17@me.com>
2018-11-18 21:45:41 +0100

    UTF8

--- work.py
+++ work.py
@@ -10,12 +10,14 @@
 # import findspark
 # findspark.init()
 
-# from pyspark.sql import *
-# from pyspark.sql.functions import *
-# from pyspark.sql.functions import min
+import sys
 
-# from pyspark.sql import SparkSession
-# from pyspark import SparkConf, SparkContext
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.functions import min
+
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
 
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
@@ -24,6 +26,7 @@ sc = spark.sparkContext
 # import seaborn as sns
 # from pyspark.sql.functions import array_contains
 
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 DATA_DIR = '/datasets/twitter_internetarchive/2017/06/16/00/'
 
