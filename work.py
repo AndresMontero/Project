39c3d38849eb93b6efeedadc4c2bc767d4f5a801
Ariel <ariel.har17@me.com>
2018-11-18 23:49:48 +0100

    Working

--- work.py
+++ work.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-import sys
-
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.functions import min
@@ -11,12 +8,11 @@ from pyspark import SparkConf, SparkContext
 spark = SparkSession.builder.getOrCreate()
 sc = spark.sparkContext
 
-sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
-
 DATA_DIR = '/datasets/twitter_internetarchive/2017/06/16/00/'
 
-posts = spark.read.json('hdfs://{dir}{name}'.format(dir=DATA_DIR, name='*'))
+posts = spark.read.json('hdfs://{dir}{name}'.format(dir=DATA_DIR, name='31.json.bz2'))
 
 # Information of the dataset
 print('TEST: ')
-print(posts.show(10))
+print(posts.count())
+print(posts.take(10))
