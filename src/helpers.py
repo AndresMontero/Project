6921d100099c05d77d3b3ac93940599e307f28f1
Ariel <ariel.har17@me.com>
2018-12-07 12:33:16 +0100

    Added helper function to generate images and standarize

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,14 @@
 # Helpers module
 import numpy as np
 from IPython.display import display
+import matplotlib.pyplot as plt
+
+
+IMG_PATH = '../assets/images/'
+GRAPH_PATH = '{}graphs/'.format(IMG_PATH)
+LABEL_SIZE = 18
+TITLE_SIZE = 25
+FIG_SIZE = (10, 6)
 
 
 def neg_to_zero(quantity):
@@ -56,3 +64,61 @@ def not_eq_ign_case(a, b):
 def uniq(a):
     return list(set(a))
 
+
+def std_plot(plot_func, figsize=FIG_SIZE, title=None, xlabel=None, ylabel=None):
+    """Wrapper function to plot.
+
+    Args:
+        title: string.
+        xlabel: X axis string.
+        ylabel: Y axis string.
+        plot_func: callback to plot
+        figsize: figure size
+    """
+
+    title_text = ''
+    title_size = TITLE_SIZE
+
+    xlabel_text = ''
+    xlabel_size = LABEL_SIZE
+
+    ylabel_text = ''
+    ylabel_size = LABEL_SIZE
+
+    if type(title) == str:
+        title_size = TITLE_SIZE
+        title_text = title
+    elif type(title) == dict:
+        title_size = title['size']
+        title_text = title['text']
+
+    if type(xlabel) == str:
+        xlabel_size = LABEL_SIZE
+        xlabel_text = xlabel
+    elif type(xlabel) == dict:
+        xlabel_size = xlabel['size']
+        xlabel_size = xlabel['text']
+
+    if type(ylabel) == str:
+        ylabel_size = LABEL_SIZE
+        ylabel_text = ylabel
+    elif type(ylabel) == dict:
+        ylabel_size = ylabel['size']
+        ylabel_size = ylabel['text']
+
+    plt.figure(figsize=figsize)
+
+    fig = plot_func()
+
+    plt.title(title_text, size=title_size)
+
+    plt.xlabel(xlabel_text, size=xlabel_size)
+
+    plt.ylabel(ylabel_text, size=ylabel_size)
+
+    # fig.show()
+
+    filename = title_text.lower().replace(' ', '_')
+
+    fig.savefig('{graph}{file}'.format(graph=GRAPH_PATH,
+                                       file=filename), bbox_inches='tight')
