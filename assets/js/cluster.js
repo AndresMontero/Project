8b054cccc317b60dbbd85fec46b6eca982bd13e0
Ariel <ariel.har17@me.com>
2018-12-16 15:09:26 +0100

    clean html

---
+++ assets/js/cluster.js
@@ -0,0 +1,137 @@
+const DATA_PATH = 'assets/csv/tsne.csv';
+const CLUSTERS = 8;
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    initializeCluster();
+});
+
+let initializeCluster = () => {
+    Plotly.d3.csv(DATA_PATH, (err, rows) => {
+            let dict = [];
+
+            for (let i = 0; i < CLUSTERS; i++) {
+                dict[i] = rows.filter((d) => {
+                    return parseInt(d.cluster) === i;
+                });
+            }
+
+            let unpack = (rows, key) => {
+                return rows.map(function (row) {
+                    return row[key];
+                });
+            };
+
+            let getRandomColor = () => {
+                const letters = '0123456789ABCDEF';
+                let color = '#';
+                for (let i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            };
+
+            let getTooltipText = (points) => {
+                return points.map((point) => {
+                    let artist_text = 'Artist : ' + point.artist_name;
+                    let track_text = 'Title : ' + point.track_title;
+                    let album_text = 'Album : ' + point.album_title;
+                    let album_year_text = 'Year : ' + point.year;
+                    let genre_text = 'Genre : ' + point.genre_parent;
+                    return artist_text + '<br>' + track_text + '<br>' + album_text +
+                        '<br>' + album_year_text + '<br>' + genre_text;
+                });
+            };
+
+
+            let data = dict.map((cluster, index) => {
+                let cluster_num = index + 1;
+                return {
+                    x: unpack(cluster, 'x'),
+                    y: unpack(cluster, 'y'),
+                    z: unpack(cluster, 'z'),
+                    mode: 'markers',
+                    marker: {
+                        size: 1.5,
+                        line: {
+                            color: getRandomColor(),
+                            width: 0.5
+                        },
+                        opacity: 0.8
+                    },
+                    type: 'scatter3d',
+                    name: 'Cluster ' + cluster_num,
+                    text: getTooltipText(cluster),
+                    hoverinfo: "text+name",
+                };
+            });
+
+            let layout = {
+                title: 'Music cluster',
+                paper_bgcolor: 'rgb(5,65,65)',
+                legend: {
+                    xanchor: 12,
+                    borderwidth: 2,
+                    font: {
+                        family: 'sans-serif',
+                        size: 15,
+                        color: 'rgb(215,215,215)'
+                    },
+                    x: 1,
+                    y: 0.5,
+                },
+                margin: {
+                    l: 0,
+                    r: 0,
+                    b: 0,
+                    t: 0,
+                    pad: 4
+                },
+                autosize: true,
+                scene: {
+                    xaxis: {
+                        range: [-10, 10],
+                        showbackground: false,
+                        zeroline: false,
+                        ticks: false,
+                        showgrid: false,
+                        showspikes: false,
+                        showticklabels: false,
+                        showtickprefix: false,
+                        showexponent: false
+                    },
+                    yaxis: {
+                        range: [-10, 10],
+                        showbackground: false,
+                        zeroline: false,
+                        ticks: false,
+                        showgrid: false,
+                        showspikes: false,
+                        showticklabels: false,
+                        showtickprefix: false,
+                        showexponent: false
+                    },
+                    zaxis: {
+                        range: [-10, 10],
+                        showbackground: false,
+                        zeroline: false,
+                        ticks: false,
+                        showgrid: false,
+                        showspikes: false,
+                        showticklabels: false,
+                        showtickprefix: false,
+                        showexponent: false
+                    }
+                }
+            };
+            console.log(data);
+            Plotly.newPlot('music_cluster', data, layout, {
+                modeBarButtons: [['toImage'], ['zoom3d'], ['pan3d'], ['resetCameraDefault3d'], ['hoverClosest3d']],
+                displaylogo: false,
+                watermark: false,
+                responsive: true,
+            });
+        }
+    );
+};
+
