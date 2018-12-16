cd2505091ffb2805a044589e547ca0f13abd4acc
Ariel <ariel.har17@me.com>
2018-12-16 22:12:15 +0100

    Responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ const ALBUMSDISTSRC = 'assets/images/graphs/distribution_of_albums_per_year_2000
 const TRACKDISTSRC = 'assets/images/graphs/distribution_of_tracks_per_year_2000-2018.png';
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    document.getElementById('albumsDistBtn').addEventListener('click',function() {
+    document.getElementById('albumsDistBtn').addEventListener('click', function () {
         document.querySelectorAll('#distDiv button').forEach((button) => {
             button.classList.remove('active');
         });
@@ -43,6 +43,37 @@ document.addEventListener("DOMContentLoaded", function (e) {
         newImg.fadeIn(500);
         return false;
     });
+
+    $("#menu a[href^='#']").on('click', function (e) {
+        // This sets the hash
+        var target;
+        target = this.hash;
+
+        // Prevent default anchor click behavior
+        e.preventDefault();
+
+        // The grabs the height of my header
+        var navOffset;
+        navOffset = $('#header').height();
+
+        // Animate The Scroll
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - navOffset - 23
+        }, 600, function () {
+            // Adds hash to end of URL
+            return window.history.pushState(null, null, target);
+        });
+    });
 });
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
 
 
